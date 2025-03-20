117f0ff992b0dde2f2b871d306d12b2a7e10df15
Marcos Suzuki <marcossuzuki@gmail.com>
2025-03-20 15:39:41 -0300

    Update Mola.py

--- Mola.py
+++ Mola.py
@@ -1,15 +1,25 @@
 from math import pi
-from typing import Generic, overload
 
 class Mola:
 
-    def __init__(self, d_arame:float, d_medio:float, d_int=None, d_ext=None, tipo=None):
-        self.__d_arame = d_arame
-        self.__d_medio = d_medio
-        self.__d_int = d_int if d_int is not None else d_medio - d_arame
-        self.__d_ext = d_ext if d_int is not None else d_medio + d_arame
+    def __init__(self, d_arame=None, d_medio=None, d_int=None, d_ext=None, tipo=None):
+        self.__d = {}
+        diametro_exist = self.__calc_diametro(arame=d_arame, medio=d_medio, int=d_int, ext=d_ext)
         self.tipo = tipo
-        self.__calc_forma()
+        if diametro_exist is not None:
+            self.__calc_forma()
+
+    def __calc_diametro(self, **kwargs):
+        not_none = 0
+        for k, val in kwargs.items():
+            if val != None:
+                not_none += 1
+                self.__d[k] = val
+                print(k, self.__d[k])
+        if not_none < 2:
+            print('Não é possível realizar o calculo')
+            return None 
+        
 
     def __calc_forma(self):
         self.__w = self.w
@@ -19,35 +29,40 @@ class Mola:
         self.__kc = self.kc
     
     @property
-    def d_arame(self): return self.__d_arame
+    def d_arame(self): 
+        return self.__d['arame']
     
     @d_arame.setter
     def d_arame(self, value):
-        self.__d_arame = value
+        self.__d['arame'] = value
         self.__calc_forma()
 
     @property
-    def d_medio(self): return self.__d_medio
+    def d_medio(self): 
+        return self.__d['medio']
     
     @d_medio.setter
     def d_medio(self, value):
-        self.__d_medio = value
+        self.__d['medio'] = value
         self.__calc_forma()
 
     @property
-    def d_int(self): return self.__d_int
+    def d_int(self): 
+        return self.__d['int']
     
     @d_int.setter
     def d_int(self, value):
-        self.__d_int = value
+        self.__d['int'] = value
         self.d_medio = self.d_int + self.d_arame
         self.__calc_forma()
     
     @property
-    def d_ext(self): return self.__d_ext
+    def d_ext(self): 
+        return self.__d['ext']
 
     @property
-    def w(self): return self.d_medio/self.d_arame
+    def w(self): 
+        return self.d_medio/self.d_arame
        
     @property
     def ks(self):
@@ -68,35 +83,35 @@ class Mola:
         return kb
     
     @property
-    def kc(self): return self.__kb/self.__ks
+    def kc(self): 
+        return self.__kb/self.__ks
 
     def calc_tensao(self, force:float):
-        w = self.w
         d_medio = self.d_medio
         d_arame = self.d_arame
-        return self.__kb*(8*force*d_medio)/(pi*d_arame**3)
+        return self.kb*(8*force*d_medio)/(pi*d_arame**3)
 
     # def __str__(self):
     #     return self.__name__
 
 
 if __name__ == '__main__':
-    mola = Mola(0.037, d_medio=.4)
-    print(mola.calc_tensao(force=6.46))
-    print(f'Indice da mola: {mola.w}')
-    print(f'Kb: {mola.kb}')
-    print(f'Kc: {mola.kc}')
-    print(f'Ks: {mola.ks}')
-    print(f'Kw: {mola.kw}')
-    mola.d_arame = 0.04
-    print(mola.d_arame)
-    print(mola.calc_tensao(force=6.46))
-    print(f'Indice da mola: {mola.w}')
-    print(f'Kb: {mola.kb}')
-    print(f'Kc: {mola.kc}')
-    print(f'Ks: {mola.ks}')
-    print(f'Kw: {mola.kw}')
-    mola2 = Mola(0.04, 0.8 )
-    print(mola2.d_ext)
-    print(mola2.d_int)
-    print(mola2.d_medio)
+    mola = Mola( d_medio=.4)
+    # print(mola.calc_tensao(force=6.46))
+    # print(f'Indice da mola: {mola.w}')
+    # print(f'Kb: {mola.kb}')
+    # print(f'Kc: {mola.kc}')
+    # print(f'Ks: {mola.ks}')
+    # print(f'Kw: {mola.kw}')
+    # mola.d_arame = 0.04
+    # print(mola.d_arame)
+    # print(mola.calc_tensao(force=6.46))
+    # print(f'Indice da mola: {mola.w}')
+    # print(f'Kb: {mola.kb}')
+    # print(f'Kc: {mola.kc}')
+    # print(f'Ks: {mola.ks}')
+    # print(f'Kw: {mola.kw}')
+    mola2 = Mola(0.04, 0.8)
+    # print(mola2.d_ext)
+    # print(mola2.d_int)
+    # print(mola2.d_medio)
